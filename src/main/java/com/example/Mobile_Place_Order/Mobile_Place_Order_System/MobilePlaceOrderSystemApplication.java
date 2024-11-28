8279c7fa5b6bd880d4cde023f004f94a7e9cf4fd
Muhammad Ibrahim <53ibrahimm@gmail.com>
2024-11-28 18:49:32 +0800

    first commit

---
+++ src/main/java/com/example/Mobile_Place_Order/Mobile_Place_Order_System/MobilePlaceOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mobile_Place_Order.Mobile_Place_Order_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePlaceOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePlaceOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mobile_Place_Order/Mobile_Place_Order_System/MobilePlaceOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mobile_Place_Order.Mobile_Place_Order_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePlaceOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
